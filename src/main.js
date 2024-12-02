aa41cb0845349e935cb73f792036028ce2ba3db9
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2024-12-02 18:10:33 +0000

    Lighting

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const scene = new THREE.Scene();
 
 // Initalize Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.setZ(30);
 
 // Initalize renderer
 const renderer = new THREE.WebGLRenderer({
@@ -16,18 +17,27 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-// move camera
-camera.position.setZ(30);
-
+// Geometry
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshStandardMaterial({
     color: 0xffa500,
-    wireframe: true,
 });
 
 const torus = new THREE.Mesh(geometry, material)
 
 scene.add(torus)
+
+// Lighting
+const pointLight = new THREE.PointLight(0xffffff, 20, 10000, 1.5)
+pointLight.position.set(2,2,2)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+
+// Add Lights
+scene.add(pointLight, ambientLight) 
+
+//const lightHelper = new THREE.PointLightHelper(pointLight);
+//scene.add(lightHelper)
 
 function main_loop() {
     requestAnimationFrame(main_loop)
