3f81209512a4e05d3a5a1085f8a7c98766793876
ChimChimLazer <118755508+ChimChimLazer@users.noreply.github.com>
2024-12-02 17:53:44 +0000

    Add Torus

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import './style.css'
 
 import * as THREE from 'three'
 
+// Set up scene
 const scene = new THREE.Scene();
 
+// Initalize Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+// Initalize renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
@@ -16,4 +19,15 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 // move camera
 camera.position.setZ(30);
 
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({
+    color: 0xffa500,
+    wireframe: true,
+});
+
+const torus = new THREE.Mesh(geometry, material)
+
+scene.add(torus)
+
+// render scene
 renderer.render(scene, camera);
