9a7e79be7d95ef93dc693c1c3e6daf6416e581ef
ChimChimLazer <jamesedwards04@gmail.com>
2024-12-20 12:01:19 +0000

    Project List Append

--- src/projects_index.js
+++ src/projects_index.js
@@ -4,11 +4,26 @@ async function fetchProjects() {
         if (!response.ok) throw new Error('Network response was not ok');
         const projects = await response.json();
 
-        alert("Projects Fetch is Working");
-        //displayProjects(projects);
+        displayProjects(projects);
     } catch (error) {
         console.error('Error fetching projects:', error);
     }
+}
+
+function displayProjects(projects){
+    const projectsList = document.getElementById('projectList');
+    projectsList.innerHTML= '';
+
+    projects.forEach(project => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div>
+                <h3>${project.Name}</h3>
+                <a href=""><img scr="${project.Image}"></a>
+            </div>
+        `
+        projectsList.appendChild(div);
+    });
 }
 
 fetchProjects();
