05a651f811570cedc05f4573a20708bb76be46ac
ChimChimLazer <jamesedwards04@gmail.com>
2024-12-23 12:30:45 +0000

    Updates

--- projects.js
+++ projects.js
@@ -7,8 +7,32 @@ document.addEventListener("DOMContentLoaded", () => {
     async function displayProjectDetails() {
         const projectName = getProjectFromUrl();
         if (projectName){
-            const project_info = document.getElementById('project_info');
-            project_info.innerHTML = `The project name is ${projectName}`;
+            const decodedProjectName = decodeURIComponent(projectName);
+            const projects = await fetch('projects.json').then(response => response.json());
+            const project = projects.find(proj => proj.Name === decodedProjectName);
+            if (project){
+                
+                const project_info = document.getElementById('project_info');
+                project_info.innerHTML = `
+                    <h1>${project.Name}</h1>
+                    <p>${project.Description}</p>
+                `;
+
+                const project_img = document.getElementById('project_image');
+                project_img.src = "images/"+ project.Image;
+
+                if (project.Video) {
+                    const project_video = document.getElementById('project_video');
+                    project_video.innerHTML = `<source src="videos/${project.Video}">`;
+
+                    // Add video title
+                    const h2 = document.createElement('h2');
+                    h2.innerHTML = `<h2>Check Out Some Gameplay!</h2>`;  
+
+                    const video_container = document.getElementById('video_container');   
+                    video_container.prepend(h2);              
+                }
+            }
         }
     }
 
